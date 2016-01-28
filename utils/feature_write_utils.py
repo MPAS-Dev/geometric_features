b5fde2f302d15467c268427bc68892b3e36bbf93
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2016-01-28 15:44:59 -0700

    Fix formatting to follow geojson standard
    This commit updates the write utils to write standard GeoJSON which
    should be parsable by external tools.

--- utils/feature_write_utils.py
+++ utils/feature_write_utils.py
@@ -60,53 +60,109 @@ def write_single_feature(feature, out_file, base_indent):#{{{
 	out_file.write('%s\t{"type": "%s",\n'%(base_indent, feature_type))
 	out_file.write('%s\t "coordinates":\n'%(base_indent))
 
-	if not feature_type == "Point":
+	if feature_type == "MultiPolygon":
+		# An array of polygons
+		# Each polygon is an array of shapes
+		# Each The first shape in a polygon is an array describing the vertices of the exterior of the polygon
+		# Each other shape in a polygon is an array describing the vertices of each hole in the interior of the polygon
+		coords_list = feature['geometry']['coordinates']
+
 		out_file.write('%s\t\t[\n'%(base_indent))
-
-	if feature_type == "MultiPolygon" or feature_type == "MultiLineString":
-		out_file.write('%s\t\t\t[\n'%(base_indent))
-		indentation = '%s\t\t\t\t'%(base_indent)
-		poly_list = feature['geometry']['coordinates']
-	elif feature_type == "Point":
-		indentation = '\t\t\t'
-		poly_list = []
-		point_list = []
-		point_list.append(feature['geometry']['coordinates'])
-		poly_list.append(point_list)
-		del point_list
-	else:
-		indentation = '\t\t\t'
-		poly_list = []
-		poly_list.append(feature['geometry']['coordinates'])
-
-	write_poly_seps = False
-
-	for poly in poly_list:
-		if write_poly_seps:
-			out_file.write('%s%s],\n'%(base_indent, indentation))
-			out_file.write('%s%s[\n'%(base_indent, indentation))
-		else:
-			write_poly_seps = True
-
-		write_comma = False
-		for coord in poly:
-			if write_comma:
-				out_file.write(',\n')
+ 		out_file.write('%s\t\t\t[\n'%(base_indent))
+ 		write_poly_seps = False
+		for poly in coords_list:
+			write_shape_seps = False
+			if write_poly_seps:
+				out_file.write('%s\t\t\t],\n'%(base_indent))
+				out_file.write('%s\t\t\t[\n'%(base_indent))
 			else:
-#				out_file.write('\n')
-				write_comma = True
+				write_poly_seps = True
 
-			out_file.write('%s%s[ %f, %f]'%(base_indent, indentation, coord[0], coord[1]))
+			out_file.write('%s\t\t\t\t[\n'%(base_indent))
+			for shape in poly:
+				if write_shape_seps:
+					out_file.write('%s\t\t\t\t],\n'%(base_indent))
+					out_file.write('%s\t\t\t\t[\n'%(base_indent))
+				else:
+					write_shape_seps = True		
+	 
+				write_coordinates(shape, out_file, '%s\t\t\t\t\t'%(base_indent))
 
-		out_file.write('\n')
-
-
-	if feature_type == "MultiPolygon" or feature_type == "MultiLineString":
-		out_file.write('%s\t\t\t]\n'%(base_indent))
-
-	if not feature_type == "Point":
+			out_file.write('%s\t\t\t\t]\n'%(base_indent))
+ 		out_file.write('%s\t\t\t]\n'%(base_indent))
 		out_file.write('%s\t\t]\n'%(base_indent))
+
+	elif feature_type == "Polygon":
+		# One large array
+		# Inside, any number of arrays
+		# First array is an array of points representing the exterior of the polygon
+		# Any after the first are arrays of points representing the holes within the polygon
+
+		coords_list = feature['geometry']['coordinates']
+
+		out_file.write('%s\t\t[\n'%(base_indent))
+ 		out_file.write('%s\t\t\t[\n'%(base_indent))
+ 		write_poly_seps = False
+ 		for shape in coords_list:
+ 			if write_poly_seps:
+ 				out_file.write('%s\t\t\t],\n'%(base_indent))
+ 				out_file.write('%s\t\t\t[\n'%(base_indent))
+ 			else:
+ 				write_poly_seps = True		
+ 
+ 			write_coordinates(shape, out_file, '%s\t\t\t\t'%(base_indent))
+ 		out_file.write('%s\t\t\t]\n'%(base_indent))
+		out_file.write('%s\t\t]\n'%(base_indent))
+
+	elif feature_type == "MultiLineString":
+		# An array of lines
+		# Each line is an array of coordinates for each line segment
+		coords_list = feature['geometry']['coordinates']
+
+		out_file.write('%s\t\t[\n'%(base_indent))
+ 		out_file.write('%s\t\t\t[\n'%(base_indent))
+ 		write_line_seps = False
+ 		for line in coords_list:
+ 			if write_line_seps:
+ 				out_file.write('%s\t\t\t],\n'%(base_indent))
+ 				out_file.write('%s\t\t\t[\n'%(base_indent))
+ 			else:
+ 				write_line_seps = True		
+ 
+ 			write_coordinates(line, out_file, '%s\t\t\t\t'%(base_indent))
+ 		out_file.write('%s\t\t\t]\n'%(base_indent))
+		out_file.write('%s\t\t]\n'%(base_indent))
+
+	elif feature_type == "LineString":
+		# An single line
+		# Made up of an array of points
+		coords_list = feature['geometry']['coordinates']
+
+		out_file.write('%s\t\t[\n'%(base_indent))
+ 		write_coordinates(coords_list, out_file, '%s\t\t\t'%(base_indent))
+		out_file.write('%s\t\t]\n'%(base_indent))
+
+	elif feature_type == "Point":
+		coords_list = feature['geometry']['coordinates']
+		out_file.write('%s\t[ %f, %f]\n'%(base_indent, coords_list[0], coords_list[1]))
+	else:
+		print "ERROR: Unsupported feature type: %s"%(feature_type)
+		sys.exit(1)
+
 	out_file.write('%s\t}\n'%(base_indent))
 	out_file.write('%s}'%(base_indent))
 
+#}}}
+
+def write_coordinates(coords, out_file, base_indent):#{{{
+	write_comma = False
+	for coord in coords:
+		if write_comma:
+			out_file.write(',\n')
+		else:
+			write_comma = True
+
+		out_file.write('%s[ %f, %f]'%(base_indent, coord[0], coord[1]))
+
+	out_file.write('\n')
 #}}}
