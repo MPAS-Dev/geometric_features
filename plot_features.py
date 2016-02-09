ab289c0309014254d47eb253cb4c2d2ca3c628ef
Phillip J. Wolfram <phillipwolfram@gmail.com>
2016-02-08 17:12:51 -0700

    Cleans up white space

--- plot_features.py
+++ plot_features.py
@@ -8,10 +8,10 @@ It requires cartopy: http://scitools.org.uk/cartopy/docs/latest/index.html
 The -f flag is used to pass in a features file that will be plotted, and the -o
 flag can optionally be used to specify the name of the image that will be
 generated.  If more than one map type is used, the name of the map type will
-be appended to the image name.  The option -m flag can be used to specify a 
-comma-separated list of map types to be plotted.  If no map type is specified, 
-all maps are used.  Possible map types are 'cyl', 'merc', 'mill', 'mill2', 
-'moll', 'moll2', 'robin', 'robin2', 'ortho', 'northpole', 
+be appended to the image name.  The option -m flag can be used to specify a
+comma-separated list of map types to be plotted.  If no map type is specified,
+all maps are used.  Possible map types are 'cyl', 'merc', 'mill', 'mill2',
+'moll', 'moll2', 'robin', 'robin2', 'ortho', 'northpole',
 'southpole', 'atlantic', 'pacific', 'americas', 'asia'
 
 Authors: Xylar Asay-Davis, Doug Jacobsen, Phillip J. Wolfram
@@ -25,7 +25,6 @@ import os.path as ospath
 
 import cartopy.crs
 import cartopy.feature
-
 
 
 def plot_base(mapType): #{{{
@@ -65,20 +64,18 @@ def plot_base(mapType): #{{{
     else:
         raise NameError("Didn't select a valid mapType!")
 
-    
     ax = plt.axes(projection=projection)
     resolution = '50m'
     ax.add_feature(cartopy.feature.NaturalEarthFeature('physical', 'land', resolution,
                    edgecolor='face', facecolor=cartopy.feature.COLORS['land']), zorder=1)
     ax.add_feature(cartopy.feature.NaturalEarthFeature('physical', 'coastline', resolution,
                    edgecolor='black', facecolor='none'), zorder=2)
-                   
-                   
+
     draw_labels = mapType in ['merc','cyl']
-    
+
     ax.gridlines(crs=cartopy.crs.PlateCarree(), draw_labels=draw_labels,
                  linewidth=0.5, color='gray', linestyle='--')
-                   
+
     if not draw_labels:
         plt.tight_layout()
 
@@ -91,11 +88,11 @@ def divide_poly_segments(points): #{{{
     dLon = inPoints[1:,0] - inPoints[0:-1,0]
     dLat = inPoints[1:,1] - inPoints[0:-1,1]
     dist = np.sqrt(dLon**2 + dLat**2)
-    
+
     longSegmentIndices = np.nonzero(dist > minDist)[0]
     if(len(longSegmentIndices) == 0):
         return inPoints
-    
+
     # start with all points up to the first long segiment
     outPoints = inPoints[0:longSegmentIndices[0],:]
     for index in range(len(longSegmentIndices)):
@@ -105,7 +102,7 @@ def divide_poly_segments(points): #{{{
         newPoints[:,0] = np.linspace(inPoints[segIndex,0], inPoints[segIndex+1,0], newPointCount)
         newPoints[:,1] = np.linspace(inPoints[segIndex,1], inPoints[segIndex+1,1], newPointCount)
         outPoints = np.append(outPoints, newPoints[0:-1,:], axis=0)
-        
+
         # now add all the points up to the next long segment, or remaining points
         # if there are no more long segments
         if(index+1 < len(longSegmentIndices)):
@@ -113,16 +110,16 @@ def divide_poly_segments(points): #{{{
         else:
             endIndex = inPoints.shape[0]
         outPoints = np.append(outPoints, inPoints[segIndex+1:endIndex,:], axis=0)
-        
+
     return outPoints #}}}
 
 def plot_poly(mapInfo, points, color, filled=True): #{{{
 
     points = divide_poly_segments(points)
     #points = np.asarray(points)
-    
+
     refProjection = cartopy.crs.PlateCarree()
-    
+
     for mapIndex in range(len(mapInfo)):
         (mapType, ax, projection, plotFileName, fig) = mapInfo[mapIndex]
         x = points[:,0]
@@ -131,11 +128,11 @@ def plot_poly(mapInfo, points, color, filled=True): #{{{
         ax.plot(x, y, transform=refProjection, color=color, linewidth=2.0, zorder=4)
 
     return #}}}
-  
+
 def plot_point(mapInfo, points, marker, color): #{{{
 
     points = np.asarray(points)
-    
+
     refProjection = cartopy.crs.PlateCarree()
 
     for mapIndex in range(len(mapInfo)):
@@ -145,7 +142,7 @@ def plot_point(mapInfo, points, marker, color): #{{{
     return #}}}
 
 def plot_features_file(featurefile, mapInfo): #{{{
-    
+
     # open up the database
     with open(featurefile) as f:
         featuredat = json.load(f)
@@ -187,7 +184,7 @@ def plot_features_file(featurefile, mapInfo): #{{{
     for mapIndex in range(len(mapInfo)):
         (mapType, ax, projection, plotFileName, fig) = mapInfo[mapIndex]
         print 'saving ' + plotFileName
-        plt.figure(mapIndex+1) 
+        plt.figure(mapIndex+1)
         plt.savefig(plotFileName)
 
     return #}}}
@@ -204,8 +201,8 @@ if __name__ == "__main__":
 
 
     if not args.map_type:
-        mapTypes = ['cyl', 'merc', 'mill', 'mill2', 
-                    'moll', 'moll2', 'robin', 'robin2', 'ortho', 'northpole', 
+        mapTypes = ['cyl', 'merc', 'mill', 'mill2',
+                    'moll', 'moll2', 'robin', 'robin2', 'ortho', 'northpole',
                     'southpole', 'atlantic', 'pacific', 'americas', 'asia']
     else:
         mapTypes = args.map_type.split(',')
