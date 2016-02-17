3e8c6390a7d68cdf82be94ead7e82225f3170d10
Xylar Asay-Davis <xylarstorm@gmail.com>
2016-02-17 01:33:06 +0100

    Add Mediterranean to Atlantic basin

--- driver_scripts/setup_MOC_basins.py
+++ driver_scripts/setup_MOC_basins.py
@@ -17,31 +17,34 @@ options, args = parser.parse_args()
 
 defaultFileName = 'features.geojson'
 
-for oceanName in ['Atlantic']:
+if os.path.exists(defaultFileName):
+    os.remove(defaultFileName)
 
+basinName = 'Atlantic'
+print " * merging features to make %s Basin"%basinName
+MOCName =  '%s_MOC.geojson'%basinName
+imageName =  '%s_MOC.png'%basinName
+basinFileName =  '%s_Basin.geojson'%basinName
+for oceanName in ['Atlantic', 'Mediterranean']:
     tag = '%s_Basin'%oceanName
-    basinFileName =  '%s_Basin.geojson'%oceanName
-    MOCName =  '%s_MOC.geojson'%oceanName
-    imageName =  '%s_MOC.png'%oceanName
     MOCMaskFileName = 'ocean/region/MOC_mask_30S/region.geojson'
-    if os.path.exists(defaultFileName):
-        os.remove(defaultFileName)
 
-    print " * merging features to make %s Basin"%oceanName
     args = ['./merge_features.py', '-d', 'ocean', '-t', tag]
     subprocess.check_call(args, env=os.environ.copy())
 
-    shutil.move(defaultFileName,basinFileName)
+shutil.move(defaultFileName,basinFileName)
 
-    print " * masking out features south of MOC region"
-    args = ['./difference_features.py', '-f', basinFileName, '-m', MOCMaskFileName]
+print " * masking out features south of MOC region"
+args = ['./difference_features.py', '-f', basinFileName, '-m', MOCMaskFileName]
+subprocess.check_call(args, env=os.environ.copy())
+
+shutil.move(defaultFileName,MOCName)
+
+if options.plot:
+    args = ['./plot_features.py', '-f', MOCName, '-o', imageName, '-m', 'cyl']
     subprocess.check_call(args, env=os.environ.copy())
 
-    shutil.move(defaultFileName,MOCName)
 
-    if options.plot:
-        args = ['./plot_features.py', '-f', MOCName, '-o', imageName, '-m', 'cyl']
-        subprocess.check_call(args, env=os.environ.copy())
 
 if os.path.exists(defaultFileName):
     os.remove(defaultFileName)
