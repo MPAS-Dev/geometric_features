8b05289a9a23031db7dd900e419c96291779a481
Xylar Asay-Davis <xylarstorm@gmail.com>
2016-09-09 23:28:37 +0200

    Add script: combine features into single feature
    The script takes a file containing one or more feature definitions,
    and a new name for the combined feature. The geometry of the
    feature definitions are combined into a single feature definition,
    which is placed in (or appended to) features.geojson.

---
+++ combine_features.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+"""
+This script takes a file containing one or more feature definitions, pointed 
+to by the -f flag, and a new name for the combined feature, provided through
+the -n flag.  The geometry of the feature definitions are combined into a
+single feature definition, which is placed in (or appended to) features.geojson.
+"""
+
+import json
+import argparse
+from collections import defaultdict
+from utils.feature_write_utils import write_all_features
+
+import os.path
+
+import shapely.geometry
+import shapely.ops
+
+import sys
+
+parser = argparse.ArgumentParser(description=__doc__,
+                                 formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("-f", "--feature_file", dest="feature_file",
+                    help="feature file with features to be combined", metavar="FILE",
+                    required=True)
+parser.add_argument("-n", "--new_feature_name", dest="new_feature_name",
+                    help="The new name of the combined feature",
+                    metavar="NAME", required=True)
+
+args = parser.parse_args()
+
+
+out_file_name = "features.geojson"
+
+features = defaultdict(list)
+
+featureExists = False
+
+if os.path.exists(out_file_name):
+    try:
+        with open(out_file_name) as f:
+            appended_file = json.load(f)
+            for feature in appended_file['features']:
+                if feature['properties']['name'] == args.new_feature_name:
+                    featureExists = True
+                features['features'].append(feature)
+            del appended_file
+    except:
+        pass
+
+if featureExists:
+    print "Warning: feature %s already in features.geojson.  Nothing to do."%args.new_feature_name
+    sys.exit(0)
+
+
+featuresToCombine = defaultdict(list)
+
+try:
+    with open(args.feature_file) as f:
+        feature_file = json.load(f)
+
+    for feature in feature_file['features']:
+        featuresToCombine['features'].append(feature)
+
+    del feature_file
+except:
+    print "Error parsing geojson file: %s"%(args.feature_file)
+    raise
+
+featureShapes = []
+authors = []
+for feature in featuresToCombine['features']:
+    featureShapes.append(shapely.geometry.shape(feature['geometry']))
+    #if not featureShapes[-1].is_valid:
+    #    print "Warning: Feature %s is 'invalid' as defined by the shapely library."%feature['properties']['name']
+    authors.append(feature['properties']['author'])
+
+combinedShape = shapely.ops.cascaded_union(featureShapes)
+
+feature = {}
+feature['properties'] = {}
+feature['properties']['name'] = args.new_feature_name
+feature['properties']['component'] = featuresToCombine['features'][0]['properties']['component']
+feature['properties']['tags'] = ''
+feature['properties']['author'] = '; '.join(list(set(authors)))
+feature['geometry'] = shapely.geometry.mapping(combinedShape)
+features['features'].append(feature)
+
+out_file = open(out_file_name, 'w')
+out_file.write('{"type": "FeatureCollection",\n')
+out_file.write(' "groupName": "enterNameHere",\n')
+out_file.write(' "features":\n')
+out_file.write('\t[\n')
+write_all_features(features, out_file, '\t\t')
+out_file.write('\n')
+out_file.write('\t]\n')
+out_file.write('}\n')
+
+# vim: foldmethod=marker ai ts=4 sts=4 et sw=4 ft=python
