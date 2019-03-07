807de4768e9445995795ecaeee7bb31c65fe78b0
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-03-06 19:19:10 -0700

    Remove incorrect license header

--- geometric_features/download.py
+++ geometric_features/download.py
@@ -1,14 +1,3 @@
-# This software is open source software available under the BSD-3 license.
-#
-# Copyright (c) 2018 Los Alamos National Security, LLC. All rights reserved.
-# Copyright (c) 2018 Lawrence Livermore National Security, LLC. All rights
-# reserved.
-# Copyright (c) 2018 UT-Battelle, LLC. All rights reserved.
-#
-# Additional copyright and license information can be found in the
-# MPAS-Analysis_LICENSE file distributed with this code, or at
-# https://raw.githubusercontent.com/MPAS-Dev/MPAS-Analysis/master/LICENSE
-
 """
 Utilities for downloading files
 """
