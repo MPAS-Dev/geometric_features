51a71f5b828d48674a77955344ef24c276f34fbd
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-05-16 09:40:15 -0600

    Remove dependency on distutil

--- geometric_features/test/__init__.py
+++ geometric_features/test/__init__.py
@@ -9,7 +9,7 @@ import warnings
 from contextlib import contextmanager
 
 import os
-from distutils import dir_util
+import shutil
 from pytest import fixture
 
 try:
@@ -51,7 +51,7 @@ def loaddatadir(request, tmpdir):
     test_dir, _ = os.path.splitext(filename)
 
     if os.path.isdir(test_dir):
-        dir_util.copy_tree(test_dir, bytes(tmpdir))
+        shutil.copy_tree(test_dir, str(tmpdir))
 
     request.cls.datadir = tmpdir
 
